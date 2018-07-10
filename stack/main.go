c84c869ec0667d2697237f23e798bca86115a7df
biig_pongsatorn <biig_pongsatorn@hotmail.com>
2018-07-10 21:51:52 +0700

    push pop stack

--- stack/main.go
+++ stack/main.go
@@ -1,18 +1,32 @@
 package main
 
+import (
+	"fmt"
+)
+
 type stack []int
 
 func main() {
 	var value stack
 	value.push(5)
+	fmt.Println("result 1 : ", value)
+	value.push(3)
+	fmt.Println("result 2 : ", value)
 	value.pop()
+	fmt.Println("result 3 : ", value)
 }
 
-func (s stack) push(a int) stack {
-
-	return s
+func (s *stack) push(a int) {
+	*s = append(*s, a)
 }
 
-func (s stack) pop() (int, error) {
-	return 0, nil
+func (s *stack) pop() error {
+	if len(*s) > 0 {
+		a := *s
+		_, a = a[len(a)-1], a[:len(a)-1]
+		*s = a
+		return nil
+	} else {
+		return nil
+	}
 }
