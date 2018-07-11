87224b3f58d1d44e8e6d2256db4afbc0bd1836a7
biig_pongsatorn <biig_pongsatorn@hotmail.com>
2018-07-11 19:33:58 +0700

    wordcount

--- map/main.go
+++ map/main.go
@@ -1,19 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
-
 func main() {
-	fmt.Printf("%#v\n", wordCount("I am Biig Pongsatorn EiEi , I am not you EiEi"))
-}
 
-func wordCount(s string) map[string]int {
-	result := make(map[string]int)
-	ar := strings.Fields(s)
-	for _, a := range ar {
-		result[a]++
-	}
-	return result
 }
